725efe7924a4f5cfa182001e7fa9ca9d1b9f72e0
unknown <umutzerencs@gmail.com>
2021-05-30 16:46:31 +0300

    app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def home():
     
     
 if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3002))
     app.run(debug=True,host = '0.0.0.0', port = port)
     
     
